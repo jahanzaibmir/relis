fb01c15152c5af4660a2a7b11e1b0d2c3da8daec
Jahanzaib <jahanzaibashraf1318@gmail.com>
2026-08-18 12:37:26 +0530

    Update gdt.c

--- arch/gdt.c
+++ arch/gdt.c
@@ -24,12 +24,12 @@ typedef struct __attribute__((packed)) {
 
 static uint64_t gdt[8] = {
     0,                  // 0x00: Null
-    0x00AF9A000000FFFF, // 0x08: Kernel CS (Base=0, Limit=4GB, P=1, DPL=0, S=1, Type=0xA, G=1, L=1)
-    0x00CF92000000FFFF, // 0x10: Kernel DS (Base=0, Limit=4GB, P=1, DPL=0, S=1, Type=0x2, G=1, B=1)
-    0x00CFF2000000FFFF, // 0x18: User DS
-    0x00AFFA000000FFFF, // 0x20: User CS
-    0,                  // 0x28: TSS (Part 1)
-    0                   // 0x30: TSS (Part 2)
+    0x00AF9A000000FFFF, 
+    0x00CF92000000FFFF, 
+    0x00CFF2000000FFFF,
+    0x00AFFA000000FFFF, 
+    0,                 
+    0                  
 };
 
 typedef struct {
@@ -40,7 +40,7 @@ typedef struct {
 static gdt_descriptor_t gdtr;
 tss_t tss;
 
-// FIX: Export physical GDT info for AP trampoline
+// Export physical GDT info for AP trampoline
 uint64_t gdtr_base;
 uint16_t gdtr_limit;
 
@@ -71,7 +71,7 @@ void gdt_init(void) {
     gdtr.limit = sizeof(gdt) - 1;
     gdtr.base  = (uint64_t)(uintptr_t)&gdt;
     
-    // FIX: Set exported variables
+    // Set exported variables
     gdtr_base = gdtr.base;
     gdtr_limit = gdtr.limit;
 
@@ -83,7 +83,7 @@ void gdt_init(void) {
     tss.rsp0 = tss_rsp0;
     tss_flush(0x28);
 }
-=======
+
 #include "gdt.h"
 #include <stdint.h>
 
@@ -112,10 +112,10 @@ static uint64_t gdt[8] = {
     0,                  // 0x00: Null
     0x00AF9A000000FFFF, 
     0x00CF92000000FFFF, 
-    0x00CFF2000000FFFF, // 0x18: User DS   (Base=0, Limit=4GB, P=1, DPL=3, S=1, Type=0x2, G=1, B=1)
-    0x00AFFA000000FFFF, // 0x20: User CS   (Base=0, Limit=4GB, P=1, DPL=3, S=1, Type=0xA, G=1, L=1)
-    0,                  // 0x28: TSS (Part 1)
-    0                   // 0x30: TSS (Part 2)
+    0x00CFF2000000FFFF, 
+    0x00AFFA000000FFFF,
+    0,                 
+    0               
 };
 
 typedef struct {
