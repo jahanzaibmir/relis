57cd02acb281157f81c7256534af4366234a29dc
Jahanzaib <jahanzaibashraf1318@gmail.com>
2026-08-18 12:45:12 +0530

    Update smp.h

--- include/relis/smp.h
+++ include/relis/smp.h
@@ -13,7 +13,7 @@ struct cpu_info {
 };
 
 extern struct cpu_info cpus[MAX_CPUS];
-extern volatile int num_cpus; // FIX: Made volatile
+extern volatile int num_cpus; 
 
 void smp_init(void);
 struct cpu_info* get_cpu(uint8_t cpu_id);
